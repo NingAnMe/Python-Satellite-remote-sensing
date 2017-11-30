1b3b3db167f008a850aad81c72d570fc6b18eabc
NingAnMe <ninganme@qq.com>
2017-11-30 21:14:04 +0800

    创建启动脚本

---
+++ manage.py
@@ -0,0 +1,11 @@
+#! /usr/bin/env python
+# coding:utf-8
+
+from blog import create_app
+from flask_script import Manager
+
+app = create_app()
+manager = Manager(app)
+
+if __name__ == '__main__':
+    manager.run()
