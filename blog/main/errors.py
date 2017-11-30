d419f8dd92f828a5800d59f4467173870de8cc1c
NingAnMe <ninganme@qq.com>
2017-11-30 21:11:40 +0800

    修改错误

--- blog/main/errors.py
+++ blog/main/errors.py
@@ -3,10 +3,10 @@
 from flask import render_template
 from . import main
 
-@main.apperrorhandler(404)
+@main.app_errorhandler(404)
 def page_not_found(e):
     return render_template('404.html'), 404
 
-@main.apperrorhandler(500)
+@main.app_errorhandler(500)
 def internal_server_error(e):
     return render_template('500.html'), 500
