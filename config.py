ce2087a6f8076bb93d31f9959c7ee67c5bc65d4a
NingAnMe <ninganme@qq.com>
2017-11-30 21:12:19 +0800

    修改配置文件

---
+++ config.py
@@ -0,0 +1,20 @@
+# coding:utf-8
+
+'''全局常量
+'''
+
+class Config(object):
+    HOSTNAME = 'localhost'
+    DATABASE = 'blog'
+    USERNAME = 'blog'
+    PASSWORD = 'blog'
+    DB_URI = 'mysql://{}:{}@{}/{}'.format(
+            USERNAME, PASSWORD, HOSTNAME, DATABASE,)
+    
+    SQLALCHEMY_DATABASE_URI = DB_URI
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    DEBUG = True
+
+    @staticmethod
+    def init_app(app):
+        pass
