048e581e17798895646a3923c7dd17c59edb57ec
NingAnMe <ninganme@qq.com>
2017-11-30 18:00:10 +0800

    修改文章模型

--- app/models.py
+++ app/models.py
@@ -5,13 +5,13 @@ from ext import db
 class Post(db.Model):
     __tablename__ = 'post'
 
-    id = db.Column(db.Integer, primary_key=Ture, )
-    title = db.Columen(db.Unicode(256), nullable=False)
-    body = db.Columen(db.UnicodeText)
+    id = db.Column(db.Integer, primary_key=Ture, autoincrement=True,)
+    title = db.Columen(db.Unicode(256), nullable=False,)
+    body = db.Columen(db.UnicodeText,)
     time_stamp = db.Columen(db.DateTime, index=True, default=datetime.utcnow,
                             nullable=False,
                             )
     time_last_modify = db.Columen(db.DateTime, default=datetime.utcnow,
                                     nullable=False,
                                   )
-    tab = db.Columen(db.Unicode(64))
+    tab = db.Columen(db.Unicode(64),)
