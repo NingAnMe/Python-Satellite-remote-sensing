fddc9a5a28bcf81d4cd117780e111bb8535ee8d7
NingAnMe <ninganme@qq.com>
2017-12-02 21:22:23 +0800

    初始化 Flask-Login

--- blog/__init__.py
+++ blog/__init__.py
@@ -2,10 +2,15 @@
 
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+
 from config import config
 
 db = SQLAlchemy()
 
+login_manager = LoginManager()
+login_manager.session_protection = 'strong'
+login_manager.login_view = 'auth.login' # 设置登录时所使用的路由
 
 def create_app(config_name):
     app = Flask(__name__)
@@ -13,8 +18,12 @@ def create_app(config_name):
     config[config_name].init_app(app)
 
     db.init_app(app)
+    login_manager.init_app(app)
 
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
+
+    from auth import auth as auth_blueprint
+    app.register_blueprint(auth_blueprint, url_prefix='/auth')
 
     return app
