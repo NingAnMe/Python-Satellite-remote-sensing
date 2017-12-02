09309ea17beaf09a719a7adf71fb1cac24348ea7
NingAnMe <ninganme@qq.com>
2017-12-02 15:19:18 +0800

    添加博客文章的固定链接

--- blog/main/views.py
+++ blog/main/views.py
@@ -11,3 +11,8 @@ from . import main
 @main.route('/')
 def index():
     return u'hello world!'
+
+@main.route('/post/<int:id>')
+def post(id):
+    post = Post.query.get_or_404(id)
+    return render_template('post.html', posts=[post])
