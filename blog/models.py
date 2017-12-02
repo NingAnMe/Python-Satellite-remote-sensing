50575e55891a5182c2e9f8b7fc2e7de077238ac3
NingAnMe <ninganme@qq.com>
2017-12-02 15:18:51 +0800

    调整代码顺序

--- blog/models.py
+++ blog/models.py
@@ -10,11 +10,11 @@ class Post(db.Model):
 
     id = db.Column(db.Integer, primary_key=True, autoincrement=True,)
     title = db.Column(db.Unicode(256), nullable=False,)
+    tab = db.Column(db.Unicode(64),)
     body = db.Column(db.UnicodeText,)
     time_stamp = db.Column(db.DateTime, index=True, default=datetime.utcnow,
                            nullable=False,
                            )
     time_last_modify = db.Column(db.DateTime, default=datetime.utcnow,
-                                 nullable=False,
-                                 )
-    tab = db.Column(db.Unicode(64),)
+                                nullable=False,
+                                )
