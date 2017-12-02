a94c3dc181ca598db4b3d59f69693b724c2bbe15
NingAnMe <ninganme@qq.com>
2017-12-02 16:07:26 +0800

    添加登录路径和添加文章路径

--- blog/main/views.py
+++ blog/main/views.py
@@ -16,3 +16,11 @@ def index():
 def post(id):
     post = Post.query.get_or_404(id)
     return render_template('post.html', posts=[post])
+
+@main.route('/login', methods['GET', 'POST'])
+def login():
+    pass
+
+@main.route('/add', methods['GET', 'POST'])
+def add_post():
+    return render_template('add_post.html', form=form)
