31100f041b3896d71c826b7eedfa0d54995b4de5
NingAnMe <ninganme@qq.com>
2017-12-02 22:18:40 +0800

    添加 CSRF 密钥

--- config.py
+++ config.py
@@ -8,7 +8,7 @@ class Config(object):
     SQLALCHEMY_COMMIT_ON_TEARDOWN = True
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
-    # Flask-WTF 需要的密钥
+    # Flask-WTF 需要的防 CSRF 密钥
     SECRET_KEY = 'please dont to guess this'
 
     # 初始化 APP
