ac0c9bdab737d661002cb27fe37d3ae1eecada29
NingAnMe <ninganme@qq.com>
2017-12-02 16:07:43 +0800

    添加用户模型

--- blog/models.py
+++ blog/models.py
@@ -2,6 +2,8 @@
 
 from datetime import datetime
 
+from werkzeug.security import generate_password_hash, check_password_hash
+
 from . import db
 
 
@@ -9,12 +11,31 @@ class Post(db.Model):
     __tablename__ = 'post'
 
     id = db.Column(db.Integer, primary_key=True, autoincrement=True,)
-    title = db.Column(db.Unicode(256), nullable=False,)
-    tab = db.Column(db.Unicode(64),)
-    body = db.Column(db.UnicodeText,)
+    title = db.Column(db.String(256), nullable=False,)
+    tab = db.Column(db.String(64),)
+    body = db.Column(db.Text,)
     time_stamp = db.Column(db.DateTime, index=True, default=datetime.utcnow,
                            nullable=False,
                            )
     time_last_modify = db.Column(db.DateTime, default=datetime.utcnow,
                                 nullable=False,
                                 )
+
+
+class User(db.Model):
+    __tablename__ = 'user'
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True,)
+    username = db.Column(db.String(128), nullable=False)
+    password_hash = db.Column(db.String(128))
+
+    @preperty
+    def password(self):
+        raise AttributeError('password is not a readable attribute')
+
+    @passwrod.setter
+    def password(self, password):
+        self.password_hash = generate_password_hash(pasword)
+
+    def verify_password(self, password):
+        return check_password_hash(self.password_hash, password)
