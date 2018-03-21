8573a44ac35d83b88fe12094651e58ff137ddc0b
NingAnMe <ninganme@qq.com>
2018-03-21 09:31:52 +0800

    删除：删除第一个教程版本，重新开发

--- blog/auth/__init__.py
+++
@@ -1,9 +0,0 @@
-# coding:utf-8
-'''blog程序的登录模块
-'''
-
-from flask import Blueprint
-
-auth = Blueprint('auth', __name__)
-
-from . import views
--- blog/main/__init__.py
+++
@@ -1,9 +0,0 @@
-# coding:utf-8
-'''blog程序主模块
-'''
-
-from flask import Blueprint
-
-main = Blueprint('main', __name__)
-
-from . import views, errors
